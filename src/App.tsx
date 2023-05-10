af51dc0df8ff6d8fc199c4e39ebcf5d312bcad56
dlopezav <38569880+dlopezav@users.noreply.github.com>
2023-05-10 20:28:43 +0200

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import logo from './assets/logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import { fetchTracks } from './lib/fetchTracks';
+import { useQuery } from '@tanstack/react-query';
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
   'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
@@ -15,7 +16,11 @@ const App = () => {
   const goToNextTrack = () => {
     setTrackIndex(trackIndex + 1);
   };
-
+  const { data: tracks } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+  console.log(tracks);
   return (
     <div className="App">
       <header className="App-header">
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { SavedTrack } from 'spotify-types';
+
+const apiToken =
+  'BQCFSi_M_CFAsnHb-2EnRj9_Hr407_E9F7C_DEyYOBzZLHPjv17rrZGfHNNd95xSP0FtrI4cBVy3yLAM5MLGHfzhOCyOilTCZaMuwhK1BxIbR4f04kAmfWXNvl5f5jk0qx3xRkBIFQ-fZddfrVCQ5DX0VvXA-c-oPv5ctqaATsUiCzaUb9FQhDuSfiFwMZCS3SUG8JIt8wI8LRBP-_LgLWE';
 
 export const fetchTracks = async () => {
-  // This function does nothing, yet
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: unknown[] };
+
+  return data.items;
 };
